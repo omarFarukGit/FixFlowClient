e0fdae48c3182c7c1422e197be02390456741bf4
MD Omar Faruk <muhammadabuhafz@gmail.com>
2026-09-17 20:08:44 +0600

    Project initialization continued - Ofetch + Tanstack

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import Providers from "@/providers";
 
 const inter = Inter({subsets:['latin'],variable:'--font-sans'});
 
@@ -24,9 +25,18 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
       lang="en"
-      className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-sans", inter.variable)}
+      className={cn(
+        "h-full",
+        "antialiased",
+        geistSans.variable,
+        geistMono.variable,
+        "font-sans",
+        inter.variable,
+      )}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <Providers>
+        <body className="min-h-full flex flex-col">{children}</body>
+      </Providers>
     </html>
   );
 }
