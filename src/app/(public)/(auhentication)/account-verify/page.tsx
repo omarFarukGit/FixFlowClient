2cf39d88db9f758e7157a19368188d68142ce908
MD Omar Faruk <muhammadabuhafz@gmail.com>
2026-09-18 06:20:05 +0600

    Setting up layered folder structure

---
+++ src/app/(public)/(auhentication)/account-verify/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function VerifyAccount() {
+  return (
+    <div>
+      verify account
+    </div>
+  )
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import { cn } from "@/lib/utils";
 import Providers from "@/providers";
 
-const inter = Inter({subsets:['latin'],variable:'--font-sans'});
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
